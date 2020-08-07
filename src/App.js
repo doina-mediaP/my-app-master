df04ce253cf196584e4b61484f10f0798bd9d3cb
Doina <palamarciuc.doina23@gmail.com>
2020-08-07 19:03:07 +0300

    improve

--- src/App.js
+++ src/App.js
@@ -1,67 +1,66 @@
-import React, {useState} from "react";
-import Unsplash, {toJson} from "unsplash-js";
-import Image from "./Image/Image";
-import "./App.scss";
-require('dotenv').config();
-process.env.CI = false;
+import React, { useState } from 'react'
+import Unsplash, { toJson } from 'unsplash-js'
+import Image from './Image/Image'
+import './App.scss'
+require('dotenv').config()
+process.env.CI = false
 
-const apiKey= process.env.REACT_APP_UNSPLASH_KEY;
-const unsplash = new Unsplash({accessKey: apiKey});
+const apiKey = process.env.REACT_APP_UNSPLASH_KEY
+const unsplash = new Unsplash({ accessKey: apiKey })
 
 const Component = () => {
-    const [searchString, setSearchString] = useState("");
-    const [images, setImages] = useState([]);
-    const [isFirstRequest, setIsFirstRequest] = useState(true);
-    const [isLoaded, setIsLoaded] = useState(false);
+  const [searchString, setSearchString] = useState('')
+  const [images, setImages] = useState([])
+  const [isFirstRequest, setIsFirstRequest] = useState(true)
+  const [isLoaded, setIsLoaded] = useState(false)
 
-    const searchImg = (e) => {
-           e.preventDefault();
-           setIsLoaded(true);
-           unsplash.search
-            .photos(searchString, 1, 10, {orientation: "portrait"})
-            .then(toJson)
-            .then((json) => {
-                setImages(json.results);
-                setIsLoaded(false);
-            });
-        setIsFirstRequest(false);
-    };
-  
-    return (
-        <div className="image-search">
-            <form onSubmit={searchImg} className="searchForm">
-                <div className="input">
-                    <input
-                        className="image-search__input"
-                        type="text"
-                        onChange={(e) => setSearchString(e.target.value)}
-                    />
-                </div>
-                <button className="image-search__btn" onClick={searchImg}>
-                    Search
-                </button>
-            </form>
+  const searchImg = (e) => {
+    e.preventDefault()
+    setIsLoaded(true)
+    unsplash.search
+      .photos(searchString, 1, 10, { orientation: 'portrait' })
+      .then(toJson)
+      .then((json) => {
+        setImages(json.results)
+        setIsLoaded(false)
+      })
+    setIsFirstRequest(false)
+  }
 
-            <div className="image-search__image-list">
-
-                {images.length > 0 || isFirstRequest ? (
-                    images.map((image, index) => {
-                        return (
-                            <Image
-                                src={image.urls.small}
-                                alt={image.alt_description}
-                                ss={index}
-                                isLoaded={isLoaded}
-                            />
-                        );
-                    })
-                ) : (
-                    <p className="noFound">No images found</p>
-                )}
-
-            </div>
+  return (
+    <div className="image-search">
+      <form onSubmit={searchImg} className="searchForm">
+        <div className="input">
+          <input
+            className="image-search__input"
+            type="text"
+            onChange={(e) => setSearchString(e.target.value)}
+          />
         </div>
-    );
-};
+        <button className="image-search__btn" onClick={searchImg}>
+          Search
+        </button>
+      </form>
 
-export default Component;
+      <div className="image-search__image-list">
+        {images.length > 0 || isFirstRequest ? (
+          images.map((image, index) => {
+            return (
+              <Image
+                  src={image.urls.small}
+                  alt={image.alt_description}
+                  ss={index}
+                  key={index}
+                  isLoaded={isLoaded}
+              />
+            )
+          })
+        ) : (
+          <p className="noFound">No images found</p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Component
--- src/Image/Image.js
+++ src/Image/Image.js
@@ -1,64 +1,21 @@
-import React from "react";
+import React from 'react'
 
-const Image = ({src, alt, ss, isLoaded}) => {
+const Image = ({ src, alt, isLoaded }) => {
+  return (
+    <>
+      <div className="img">
+        {isLoaded ? (
+          <div className="lds-spinner">
+              {new Array(12).map((index) => <div key={index+Math.random()} />)}
+          </div>
+        ) : (
+          <img className="image-search__image" src={src} alt={alt} />
+        )}
+      </div>
+    </>
+  )
+}
 
-    return (
-        <>
-            <div className="img" key={ss}>
-                {
-                    isLoaded 
-                    ?
-                    <div className="lds-spinner">
-                        {new Array(12).fill(true).map((index) => <div key={index} />)}
-                    </div>
-                    :
-                    <img
-                        className="image-search__image"
-                        src={src}
-                        alt={alt}
-                    />
-                    
-                }
-            </div>            
-        </>
-    );
-};
-
-export default Image;
-
-
-
-// import React, {useState} from "react";
-
-// const Image = ({src, alt}) => {
-//     const [isLoaded, setIsLoaded] = useState(false);
-//     const imageLoaded = () => setIsLoaded(true);
-  
-//     return (
-//         <React.Fragment>
-//             <div className="img">
-//                 <img
-//                     className={
-//                         isLoaded
-//                             ? "image-search__image" 
-//                             : "image-search__image" 
-//                     }
-//                     src={src}
-//                     alt={alt}
-//                     key={alt}
-//                     onLoad={imageLoaded}
-//                 />
-//                  <div className={!isLoaded ? "lds-spinner visible" : "lds-spinner hidden"}>
-//                 {new Array(12).fill(true).map(() => <div/>)}
-//             </div> 
-//             </div>
-         
-           
-            
-//         </React.Fragment>
-//     );
-// };
-
-// export default Image;
+export default Image
 
 
